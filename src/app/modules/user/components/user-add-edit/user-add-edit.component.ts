650137085fd319c8358993bb4696a82c88d704c9
yash donda <yashdonda007@gmail.com>
2023-07-10 17:44:48 +0530

    list of user

--- src/app/modules/user/components/user-add-edit/user-add-edit.component.ts
+++ src/app/modules/user/components/user-add-edit/user-add-edit.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../model/user.model';
+import { userRole } from 'src/shared/constants/user-role';
+import { Country, State, City } from 'src/app/interfaces/country-state-city';
+import { DataService } from 'src/shared/services/data.service';
+import { AlertService } from 'src/app/alerts/alert.service';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../../service/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-user-add-edit',
@@ -8,15 +17,97 @@ import { User } from '../../model/user.model';
 })
 export class UserAddEditComponent implements OnInit {
 
-  user = new User()
+  @ViewChild('userForm') userForm!: NgForm;
+  user = new User();
+  userRole = userRole;
+  selectedCountryId!: string;
+  countries!: Country[];
+  states!: State[];
+  cities!: City[];
+  selectedCountry = this.user.country;
+  selectedState: any;
+  userId: number | undefined;
+  prevdata: any;
 
-  constructor() { }
+
+  constructor(
+    private dataService: DataService,
+    private alertService: AlertService,
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+
+  ) { }
 
   ngOnInit(): void {
+    this.countries = this.dataService.getCountries();
+    this.userId = Number(this.route.snapshot.params['id']);
+
+    if (this.userId) {
+      this.populateForm(this.userId);
+    }
   }
 
-  submit(){
+  onCountryChange(type: string | null) {
+    this.selectedCountry = this.user.country;
+    if (this.selectedCountry) {
+      this.selectedState = null;
+      this.states = this.dataService.getStates(this.selectedCountry);
+    } else {
+      this.states = [];
+    }
+    if (!type) {
+      this.user.state = '';
+      this.user.city = '';
+    }
+
+  }
+
+  onStateChange(type: string | null) {
+    this.selectedState = this.user.state;
+    if (this.selectedState) {
+      this.cities = this.dataService.getCities(this.selectedState);
+    } else {
+      this.cities = [];
+    }
+    if (!type) {
+      this.user.city = '';
+    }
+  }
+
+  submit(userForm: NgForm) {
+
+    if (userForm.valid) {
+      const { confirmPass, ...formData } = this.user;
+      console.log(formData);
+      this.userService.saveUser(formData);
+      this.router.navigate(['/user']);
+      this.alertService.showAlert('user Added Successfully', 'success')
+    } else {
+      this.alertService.showAlert('Please fill all details correctly!', 'error')
+    }
+
+  }
+
+  populateForm(uId: number) {
+    this.prevdata = this.userService.getdata(uId);
+    this.user = this.prevdata;
+    this.onCountryChange('update');
+    this.onStateChange('update');
     console.log(this.user)
+  }
+
+  update(userForm: NgForm) {
+    if (userForm.valid) {
+      if (this.userId) {
+        this.userService.update(userForm.value, this.userId);
+      } else {
+        this.alertService.showAlert('Please fill all details correctly!', 'error')
+      }
+    }
+  }
+  check(event: Event) {
+    console.log(event)
   }
 
 }
